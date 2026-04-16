3cf704e68a2b470c12c9bc41f2089a45a3a39525
eric2458 <59672148+eric2458@users.noreply.github.com>
2026-04-16 08:51:42 -0700

    Updated movement
    Updated movement controlls as well as added cinemachine to follow object

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,14 +3,20 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 7f;
+    [SerializeField] private float rotateSpeed = 100f;
     [SerializeField] private GameInput gameInput;
     private void Update()
     {
         Vector2 inputVector = gameInput.GetMovementVectorNormalized();
+        
+        float moveAmount = -inputVector.y;
+        float turnAmount = -inputVector.x;
 
-        Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
-        transform.position += moveDir * moveSpeed * Time.deltaTime;
-        float rotateSpeed = 10f;
-        transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotateSpeed);
+        transform.position += transform.forward * moveAmount * moveSpeed * Time.deltaTime;
+        transform.Rotate(0f, turnAmount * rotateSpeed * Time.deltaTime, 0f);
+        //Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
+        //transform.position += moveDir * moveSpeed * Time.deltaTime;
+        //float rotateSpeed = 10f;
+        //transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotateSpeed);
     }
 }
