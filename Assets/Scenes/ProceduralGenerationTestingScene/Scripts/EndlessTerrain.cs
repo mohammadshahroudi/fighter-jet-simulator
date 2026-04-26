467a6b371b140fa6a962f4f97e7c428caae1cc15
Alan Alvarez <alvarezalanjose@gmail.com>
2026-04-25 22:18:25 -0700

    Commit - finished part 10/21 of the procedural terrain tutorial
    Terrain generation doesn't have those gaps between chunks when looking at the height difference. Well they still do but it ain't as obvious as before.
    
    It's getting more heavy on the performance side. Hoping to get some optimizations later on.
    
    Also I will leave this here for future reference:
    	// Not making this serializable but if we want to control how big the overall the region of generated chunks is then this variable is the right one to change.
    	const float scale = 4f; // 5 was the default but 4 is good for now.

--- Assets/Scenes/ProceduralGenerationTestingScene/Scripts/EndlessTerrain.cs
+++ Assets/Scenes/ProceduralGenerationTestingScene/Scripts/EndlessTerrain.cs
@@ -4,6 +4,9 @@ using System.Collections.Generic;
 
 public class EndlessTerrain : MonoBehaviour {
 
+	// Not making this serializable but if we want to control how big the overall the region of generated chunks is then this variable is the right one to change.
+	const float scale = 4f; // 5 was the default but 4 is good for now.
+
 	const float viewerMoveThresholdForChunkUpdate = 25f;
 	const float sqrViewerMoveThresholdForChunkUpdate = viewerMoveThresholdForChunkUpdate * viewerMoveThresholdForChunkUpdate;
 
@@ -20,7 +23,7 @@ public class EndlessTerrain : MonoBehaviour {
 	int chunksVisibleInViewDst;
 
 	Dictionary<Vector2, TerrainChunk> terrainChunkDictionary = new Dictionary<Vector2, TerrainChunk>();
-	List<TerrainChunk> terrainChunksVisibleLastUpdate = new List<TerrainChunk>();
+	static List<TerrainChunk> terrainChunksVisibleLastUpdate = new List<TerrainChunk>();
 
 	void Start() {
 		mapGenerator = FindObjectOfType<MapGenerator> ();
@@ -33,7 +36,7 @@ public class EndlessTerrain : MonoBehaviour {
 	}
 
 	void Update() {
-		viewerPosition = new Vector2 (viewer.position.x, viewer.position.z);
+		viewerPosition = new Vector2 (viewer.position.x, viewer.position.z) / scale;
 
 		if ((viewerPositionOld - viewerPosition).sqrMagnitude > sqrViewerMoveThresholdForChunkUpdate) {
 			viewerPositionOld = viewerPosition;
@@ -57,9 +60,6 @@ public class EndlessTerrain : MonoBehaviour {
 
 				if (terrainChunkDictionary.ContainsKey (viewedChunkCoord)) {
 					terrainChunkDictionary [viewedChunkCoord].UpdateTerrainChunk ();
-					if (terrainChunkDictionary [viewedChunkCoord].IsVisible ()) {
-						terrainChunksVisibleLastUpdate.Add (terrainChunkDictionary [viewedChunkCoord]);
-					}
 				} else {
 					terrainChunkDictionary.Add (viewedChunkCoord, new TerrainChunk (viewedChunkCoord, chunkSize, detailLevels, transform, mapMaterial));
 				}
@@ -96,8 +96,9 @@ public class EndlessTerrain : MonoBehaviour {
 			meshFilter = meshObject.AddComponent<MeshFilter>();
 			meshRenderer.material = material;
 
-			meshObject.transform.position = positionV3;
+			meshObject.transform.position = positionV3 * scale;
 			meshObject.transform.parent = parent;
+			meshObject.transform.localScale = Vector3.one * scale;
 			SetVisible(false);
 
 			lodMeshes = new LODMesh[detailLevels.Length];
@@ -145,6 +146,8 @@ public class EndlessTerrain : MonoBehaviour {
 							lodMesh.RequestMesh (mapData);
 						}
 					}
+
+					terrainChunksVisibleLastUpdate.Add (this);
 				}
 
 				SetVisible (visible);
