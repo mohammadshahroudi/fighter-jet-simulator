d43b829b122e30ab15945711e768c47cf706dc9f
CYB101DMartinez14 <david.martinez8312@gmail.com>
2026-04-29 22:12:04 -0700

    Stuff

--- Assets/Scripts/Player/PlayerStats.cs
+++ Assets/Scripts/Player/PlayerStats.cs
@@ -1,8 +1,18 @@
 using UnityEngine;
-using System.Collections;
-using System.Collections.Generic;
-public class PlayerStats : MonoBehaviour
+
+public class PlayerStats : MonoBehaviour, IDamageable
 {
     public float health = 100f;
 
+    public void TakeDamage(float amount)
+    {
+        health = Mathf.Max(0f, health - amount);
+        Debug.Log($"Player took {amount} damage. Remaining health: {health}");
+
+        if (health <= 0f)
+        {
+            GameStateManager.Instance?.TriggerGameOver();
+            Destroy(gameObject);
+        }
+    }
 }
