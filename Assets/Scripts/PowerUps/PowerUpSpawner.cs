d0ee92edc968d3b7554affd34bfb77b25d1ddc93
eric2458 <59672148+eric2458@users.noreply.github.com>
2026-05-06 10:49:27 -0700

    Update spawners
    Spawnnig infront of player

--- Assets/Scripts/PowerUps/PowerUpSpawner.cs
+++ Assets/Scripts/PowerUps/PowerUpSpawner.cs
@@ -5,6 +5,7 @@ public class SpawnManager : MonoBehaviour
 {
     [SerializeField] private GameObject[] powerUpPrefabs;
     [SerializeField] private float spawnInterval = 5f;
+    [SerializeField] private float powerUpLifeTime = 15f;
 
     [Header("Spawn Settings")]
     [SerializeField] private Transform player;
@@ -12,24 +13,51 @@ public class SpawnManager : MonoBehaviour
     [SerializeField] private float sideRange = 50f;
     [SerializeField] private float heightRange = 30f;
 
-    void Start() {
+    private Rigidbody playerRb;
+    void Start() 
+    {
+        if (player != null)
+        {
+            playerRb = player.GetComponent<Rigidbody>();
+        }
         StartCoroutine(SpawnPowerUpRoutine());
     }
 
-    IEnumerator SpawnPowerUpRoutine() {
-        while (true) {
+    IEnumerator SpawnPowerUpRoutine()
+    {
+        while (true)
+        {
             yield return new WaitForSeconds(spawnInterval);
-            if(player == null) continue;
-            
-            Vector3 spawnPos = player.position + player.forward * forwardDistance;
-            
-            spawnPos += player.right * Random.Range(-sideRange, sideRange);
-            spawnPos += player.up * Random.Range(-heightRange, heightRange);
+            if (player == null || powerUpPrefabs.Length == 0)
+            {
+                continue;
+            }
 
-            int randomIndex = Random.Range(0, powerUpPrefabs.Length);
-
-            Instantiate(powerUpPrefabs[randomIndex], spawnPos, Quaternion.identity);
-
+            SpawnPowerUp();
         }
     }
+    
+    private void SpawnPowerUp()
+    {
+        Vector3 forwardDirection = player.forward;
+        if (playerRb != null && playerRb.linearVelocity.sqrMagnitude > 1f)
+        {
+            forwardDirection = playerRb.linearVelocity.normalized;
+        }
+        Vector3 spawnPos = player.position + player.forward * forwardDistance; 
+        
+        spawnPos += player.right * Random.Range(-sideRange, sideRange);
+        spawnPos += player.up * Random.Range(-heightRange, heightRange);
+        
+        int randomIndex = Random.Range(0, powerUpPrefabs.Length);
+
+        //Instantiate(powerUpPrefabs[randomIndex], spawnPos, Quaternion.identity);
+
+        GameObject powerUp = Instantiate(
+            powerUpPrefabs[randomIndex],
+            spawnPos,
+            Quaternion.identity);
+        Destroy(powerUp, powerUpLifeTime);
+        
+    } 
 }
