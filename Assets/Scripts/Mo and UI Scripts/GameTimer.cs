aa8c62225ccf2cd5152a9de19a8739ae25e120d8
MohammadShahroudi <149831372+MohammadShahroudi@users.noreply.github.com>
2026-05-06 11:33:11 -0700

    updated_timer

--- Assets/Scripts/Mo and UI Scripts/GameTimer.cs
+++ Assets/Scripts/Mo and UI Scripts/GameTimer.cs
@@ -2,8 +2,9 @@ using UnityEngine;
 
 public class GameTimer : MonoBehaviour
 {
-   private static float timeLeft = 10f;
+   private static float timeLeft = 30f;
    private bool timerActive = true;
+   private static float timerCounter;
    // float seconds;
 
    public void StartTimer()
@@ -22,13 +23,27 @@ public class GameTimer : MonoBehaviour
       {
          timeLeft -= Time.deltaTime;
          Debug.Log("Time left: " + (int) timeLeft);
+         
+         if (timeLeft <= 0)
+         {
+            Debug.Log("Game Over!");
+            StopTimer();
+         }
+
+         // This is supposed to add about 30 seconds if the player 
+         // kills an enemy
+         if (timeLeft <= 20)
+         {
+            timeLeft += 30f;
+         }
+         
+         // This is supposed to be if the player 
+         // survives for about 300 seconds which is equivalent
+         // to 5 minutes then
+         // spawn the boss
       }
 
-      if (timeLeft <= 0)
-      {
-         Debug.Log("Game Over!");
-         StopTimer();
-      }
+      
    }
 }
 
