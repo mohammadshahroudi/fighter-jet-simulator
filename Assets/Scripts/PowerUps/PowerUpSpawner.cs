11260235e0a58bc8d0ba945e4095b273218e4c23
eric2458 <59672148+eric2458@users.noreply.github.com>
2026-05-06 09:37:54 -0700

    PowerUps Spawner
    Made it so power ups spawn infront of player at random

--- Assets/Scripts/PowerUps/PowerUpSpawner.cs
+++ Assets/Scripts/PowerUps/PowerUpSpawner.cs
@@ -1,9 +1,16 @@
 using System.Collections;
 using UnityEngine;
 
-public class SpawnManager : MonoBehaviour {
-    public GameObject[] powerUpPrefabs;
-    public float spawnInterval = 5.0f;
+public class SpawnManager : MonoBehaviour 
+{
+    [SerializeField] private GameObject[] powerUpPrefabs;
+    [SerializeField] private float spawnInterval = 5f;
+
+    [Header("Spawn Settings")]
+    [SerializeField] private Transform player;
+    [SerializeField] private float forwardDistance = 200f;
+    [SerializeField] private float sideRange = 50f;
+    [SerializeField] private float heightRange = 30f;
 
     void Start() {
         StartCoroutine(SpawnPowerUpRoutine());
@@ -12,12 +19,17 @@ public class SpawnManager : MonoBehaviour {
     IEnumerator SpawnPowerUpRoutine() {
         while (true) {
             yield return new WaitForSeconds(spawnInterval);
+            if(player == null) continue;
             
-            Vector3 spawnPos = new Vector3(Random.Range(-140f, 140f), 128f, -50f);
+            Vector3 spawnPos = player.position + player.forward * forwardDistance;
             
+            spawnPos += player.right * Random.Range(-sideRange, sideRange);
+            spawnPos += player.up * Random.Range(-heightRange, heightRange);
+
             int randomIndex = Random.Range(0, powerUpPrefabs.Length);
-            
+
             Instantiate(powerUpPrefabs[randomIndex], spawnPos, Quaternion.identity);
+
         }
     }
 }
