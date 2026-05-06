a38034df719a98049100605d67e9d3fc6436c645
CYB101DMartinez14 <david.martinez8312@gmail.com>
2026-05-05 23:08:07 -0700

    Added Plane Stamina Controls for the plane boost

--- Assets/Scripts/Player/PlaneStaminaUI.cs
+++ Assets/Scripts/Player/PlaneStaminaUI.cs
@@ -1,16 +1,47 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlaneStaminaUI : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [Header("References")]
+    [SerializeField] private PlayerController playerController;
+    [SerializeField] private Image barImage;
+
+    private void Awake()
     {
-        
+        if (playerController == null)
+        {
+            playerController = FindFirstObjectByType<PlayerController>();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnEnable()
     {
-        
+        if (playerController != null)
+        {
+            playerController.onStaminaChanged.AddListener(HandleStaminaChanged);
+        }
+    }
+
+    private void Start()
+    {
+        if (playerController != null && barImage != null)
+        {
+            barImage.fillAmount = 1f;
+        }
+    }
+
+    private void OnDisable()
+    {
+        if (playerController != null)
+        {
+            playerController.onStaminaChanged.RemoveListener(HandleStaminaChanged);
+        }
+    }
+
+    private void HandleStaminaChanged(float staminaNormalized)
+    {
+        if (barImage == null) return;
+        barImage.fillAmount = Mathf.Clamp01(staminaNormalized);
     }
 }
